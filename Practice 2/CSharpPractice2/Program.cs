d29e87418b86f6e4c7e24465c2dc1932d96a068d
bazinga94 <bazinga940@gmail.com>
2024-01-30 17:22:48 -0500

    Create new practice project

---
+++ Practice 2/CSharpPractice2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    class Program
+    {
+        // Generic class
+        class MyClass<T>
+        {
+            T[] arr = new T[10];
+
+            public T GetItem(int i)
+            {
+                return arr[i];
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            object obj = 3;     // object is reference type
+            int num = (int)obj; // int already inherits from object
+
+            MyClass<int> class_a = new MyClass<int>();
+            MyClass<string> class_b = new MyClass<string>();
+        }
+    }
+}
